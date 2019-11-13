6f4ba6e027c25c6855beada3f9cdfef2fbcb1f57
Murasaki-1102 <c011729114@edu.teu.ac.jp>
2019-11-13 19:54:47 +0900

    img add link

--- index.php
+++ index.php
@@ -29,11 +29,6 @@ define("MAX","24");
 $data_num  = count($sort);
 $max_page = ceil($data_num / MAX);
 
-
-function d () {
-
-}
-
 $start_number = ($now - 1) * MAX;
 
 $display_data = array_slice($sort,$start_number,MAX, true);
@@ -70,7 +65,7 @@ $display_data = array_slice($sort,$start_number,MAX, true);
           <?php foreach ($display_data as $val) : ?>
             <div class="col-lg-4 col-md-4 col-sm-6 filter <?php echo $val->getCategory() ?>">
               <div class="card main-contents">
-                <img class="card-img-top" src="<?php echo $val->getImg1() ?>">
+                <a href="/main/show.php?name=<?php echo $val->getName() ?>"><img class="card-img-top" src="<?php echo $val->getImg1() ?>"></a>
                 <div class="card-body">
                   <p class="card-text"><?php echo $val->getName() ?></p>
                   <div class="d-flex justify-content-between align-items-center">
@@ -121,6 +116,5 @@ $display_data = array_slice($sort,$start_number,MAX, true);
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <!-- <script type="text/javascript" src="/js/sort.js"></script> -->
 </body>
 </html>
